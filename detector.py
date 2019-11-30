0229cfa43bfb4f6ae37fc2eae68e6a684e464b27
Arun Chaudhary <arun.11601678@lpu.in>
2019-11-30 18:49:46 +0530

    Improved log analysis

--- detector.py
+++ detector.py
@@ -6,6 +6,25 @@ import json
 def gogo(infile_name):
     parsed_uri = []
     all_logs = []
+    pretty_logs = []
+
+    def log_parser(logfile):
+        ip_regex = r"^\d{1,3}\W\d{1,3}\W\d{1,3}\W\d{1,3}"
+        date_regex = r"\[(.*?)\]"
+        uri_regex = r'"(.*?)"'
+        response_regex = r"\"\s\d{3}"
+        bytes_regex = r"\"\s\d{3}\s\d{0,6}"
+        temp = {}
+        for logs in logfile:
+            ip = re.findall(ip_regex, logs)[0]
+            date = re.findall(date_regex, logs)[0]
+            uri = re.findall(uri_regex, logs)[0]
+            useragent = re.findall(uri_regex, logs)[2]
+            response_code = re.findall(response_regex, logs)[0].split()[1]
+            resbytes = re.findall(bytes_regex, logs)[0].split()[2]
+            temp = {'IP': ip, 'Date': date, 'Request': uri, 'User-Agent': useragent, 'Response': response_code, 'Transferred': resbytes}
+            pretty_logs.append(temp)
+        #print(pretty_logs)
 
     #log cleaning with regex
     def uri_parser(logfile):
@@ -16,7 +35,6 @@ def gogo(infile_name):
                 parsed_uri.append(re.findall(regex, line)[0][4:-9])
             elif 'POST' in line:
                 parsed_uri.append(re.findall(regex, line)[0][5:-9])
-        #print(parsed_uri)
 
     #load models
     lgs = joblib.load('trainedmodel.pkl')
@@ -25,7 +43,8 @@ def gogo(infile_name):
     #do the magic
     infile = open(infile_name, 'r')
     uri_parser(infile)
-    
+    log_parser(open(infile_name, 'r'))
+
     #list of dictionaries
     output = []
     X_predict = parsed_uri
@@ -37,10 +56,9 @@ def gogo(infile_name):
     for _ in prediction:
         temp = {}
         if _ == 0:
-            temp = { all_logs[i] : 'Clean' }
+            pretty_logs[i].update({'Verdict': 'Clean'})
         elif _ == 1:
-            temp = { all_logs[i] : ' Malicious' }
-        output.append(temp)
+            pretty_logs[i].update({'Verdict': 'Malicious'})
         i += 1
 
-    return json.dumps(output)
+    return json.dumps(pretty_logs)
