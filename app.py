6a7d9c49e6b6751badaf85b6360a081c40619173
Arun Chaudhary <arun.11601678@lpu.in>
2019-11-30 12:38:57 +0530

    Add yara rules and make new routes + views

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from detector import gogo
 import json
+from malwareanalysis import malwarescanner
 
 app = Flask(__name__)
 app.debug = True
@@ -10,11 +11,30 @@ app.secret_key = 'devkey'
 def hello():
     return "Hello World!"
 
-@app.route('/detector')
+@app.route('/logscan')
 def detector():
-    data = json.loads(gogo('access.log'))
+    return render_template('logscan.html')
+
+@app.route('/malwarescan')
+def malware():
     #data = gogo('access.log')
-    return render_template('index.html', data=data)
+    return render_template('malwarescan.html', data=data)
+
+@app.route('/loghandler', methods = ['GET', 'POST'])
+def loghandler():
+   if request.method == 'POST':
+      f = request.files['file']
+      data = json.loads(gogo(f))
+      #f.save(secure_filename(f.filename))
+      return render_template('logresult.html', data=data)
+
+@app.route('/malwarehandler', methods = ['GET', 'POST'])
+def malwarehandler():
+   if request.method == 'POST':
+      f = request.files['file']
+      data = malwarescanner(f)
+      #f.save(secure_filename(f.filename))
+      return render_template('malwareresult.html', data=data)
 
 if __name__ == '__main__':
     app.run()
